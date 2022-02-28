1ea5da7a67cc7edf1eebd410e845db84ba56fbda
xiraynedev <xiraynedev@gmail.com>
2022-02-28 12:45:56 -0500

    initial commit with webpack setup

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,7 @@
+import {FC} from 'react';
+
+export const Container: FC = () => {
+  return (
+    <div></div>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import {render} from 'react-dom';
+import {Container} from './components/Container/Container';
+import './global.scss';
+
+render(<Container />, document.getElementById('root'));

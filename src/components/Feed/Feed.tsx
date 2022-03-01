bd7170d0d240924519087c399172e92a66d75b12
Ray <xiraynedev@gmail.com>
2022-03-01 12:49:47 -0500

    add share component

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,22 +1,10 @@
 import {FC} from 'react';
-import {ShareIcon} from '../ShareIcon/ShareIcon';
-
-const person1ProfilePicture = require('../../assets/persons/person1-profile.webp');
+import {Share} from '../Share/Share';
 
 export const Feed: FC = () => {
   return (
       <section className="col-start-4 col-end-10">
-        <div className="m-4 border p-4 rounded-lg shadow-lg mt-6 ml-6">
-          <div className="flex items-center gap-3">
-            <img
-              src={person1ProfilePicture}
-              alt="person 1 profile picture"
-              className="rounded-full"
-            />
-            <p className="text-gray-500 text-lg">What is for lunch today?</p>
-          </div>
-          <ShareIcon/>
-        </div>
+       <Share />
       </section>
   );
 };
---
+++ src/components/Share/Share.tsx
@@ -0,0 +1,20 @@
+import {FC} from 'react';
+import {ShareIcon} from '../ShareIcon/ShareIcon';
+
+const person1ProfilePicture = require('../../assets/persons/person1-profile.webp');
+
+export const Share: FC = () => {
+  return (
+    <div className="m-4 border p-4 rounded-lg shadow-lg mt-6 ml-6">
+      <div className="flex items-center gap-3">
+        <img
+          src={person1ProfilePicture}
+          alt="person 1 profile picture"
+          className="rounded-full"
+        />
+        <p className="text-gray-500 text-lg">What is for lunch today?</p>
+      </div>
+      <ShareIcon/>
+    </div>
+  );
+};

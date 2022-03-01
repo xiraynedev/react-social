b3744e448d6360d4ccad1e587b24ac4724e12eca
xiraynedev <xiraynedev@gmail.com>
2022-03-01 15:57:37 -0500

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import {FC} from 'react';
 import {Home} from './pages/Home/Home';
+import {TopBar} from './components/TopBar/TopBar';
 
 export const App: FC = () => {
   return (
     <>
+      <TopBar />
       <Home />
     </>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,4 @@
 import {FC} from 'react';
-import {TopBar} from '../../components/TopBar/TopBar';
 import {LeftSidebar} from '../../components/LeftSidebar/LeftSidebar';
 import {Feed} from '../../components/Feed/Feed';
 import {RightSidebar} from '../../components/RightSidebar/RightSidebar';
@@ -7,7 +6,6 @@ import {RightSidebar} from '../../components/RightSidebar/RightSidebar';
 export const Home: FC = () => {
   return (
     <>
-      <TopBar />
       <div className='grid grid-cols-12'>
         <LeftSidebar />
         <Feed />
